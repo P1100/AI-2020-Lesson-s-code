71c7c7b12823d8e7649560c054230f58c1737e93
Pj-3 <51178702+pj-3@users.noreply.github.com>
2020-04-15 16:57:21 +0200

    Ai 2020 - Code from r14-r15

---
+++ 8.docker1/src/test/java/it/polito/ai/docker1/Docker1ApplicationTests.java
@@ -0,0 +1,12 @@
+package it.polito.ai.docker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
